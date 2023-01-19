5d4cf60555777dd01518be9a577b313468b92049
Louis LeGars <XYDistrict@gmail.com>
2023-01-19 15:54:00 +0100

    header + banner

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import logo from '../assets/images/LOGO.svg'
 import Nav from './Nav'
 import '../style/Header.css'
-
 export default function Header() {
   return (
 		<header className="headerWrap">
 			<figure className='headerWrap__fig'>
-				<img className="logo" src={logo} alt="logo de l'agence de location kasa" />
+				<img className="logo" src={logo} alt="logo de l'agence kasa" />
 			</figure>
 			<Nav className="nav-header" />
-			<h1>blblblblb</h1>
 		</header>
 	)
 }
--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div>
+        <h1>ERROR 404</h1>
+        </div>
   )
 }
