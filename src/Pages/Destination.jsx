9cd81cbbed3722973d5790ef6b9827e9b179fd38
Louis LeGars <XYDistrict@gmail.com>
2023-01-19 15:32:05 +0100

    desti.jsx

--- src/Pages/Destination.jsx
+++ src/Pages/Destination.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {useParams}  from 'react-router-dom'
+
 
 export default function FicheLogement() {
+
+  const params = useParams() 
+  console.log(params);
+
   return (
     <div>
-      <h1>Les desti dispo</h1>
+      <h1>voici la fiche Logement</h1>
     </div>
   )
 }
