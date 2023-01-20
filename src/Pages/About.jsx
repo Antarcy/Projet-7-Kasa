01d135a856404330c356554ed01b0bf13636592b
Louis LeGars <XYDistrict@gmail.com>
2023-01-20 15:15:06 +0100

    About v1.0

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import AboutBanner from "../Components/AboutBanner";
 import Collapse from "../Components/Collapse"
+
+
 
 export default function About() {
 	return (
 		<div>
+			< AboutBanner />
 			< Collapse />
 		</div>
 	);
