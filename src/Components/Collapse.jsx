8935afd9268c41ae4609f67190f8cf37044259aa
Louis LeGars <XYDistrict@gmail.com>
2023-01-20 15:22:07 +0100

    className chevron pour rotation

--- src/Components/Collapse.jsx
+++ src/Components/Collapse.jsx
@@ -3,22 +3,23 @@ import '../style/Collapse.scss'
 import Chevron from '../assets/images/vectorBas.svg'
 
 export default function Collapse(props) {
-
   const [toggle, setToggle] = useState(false)
   const [heightEl, setHeightEl] = useState()
   const toggleState = () => {
     setToggle(!toggle)
   }
   const refHeight = useRef()
+
   useEffect(() => {
     setHeightEl(`${refHeight.current.scrollHeight}px`)
   }, [])
+
   return (
 		<div className="collapse-about">
 			<div className="collapseContainer">
 				<div onClick={toggleState} className="collapse-about__visible">
 					<h2>{props.aboutTitle}</h2>
-					<img src={Chevron} alt="chevron down" />
+					<img className='aboutChevron' src={Chevron} alt="chevron down" />
 				</div>
 			</div>
 			<div
