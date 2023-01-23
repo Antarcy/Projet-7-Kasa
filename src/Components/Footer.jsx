60b2e9b056c3f1d6931f19b11bc019e895bb0b5f
Louis LeGars <XYDistrict@gmail.com>
2023-01-23 14:07:47 +0100

    Footer scss fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,10 +3,10 @@ import logo from '../assets/images/logo-footer.svg'
 import '../style/main.scss'
 
 export default function Footer() {
-  return (
-		<footer className="container">
-			<img src={logo} alt="logo de kasa" />
-			<p className="container__text">© 2020 Kasa. All rights reserved</p>
-		</footer>
-	);
-}
+	return (
+		  <footer className="foot-container">
+			  <img src={logo} alt="logo de kasa" />
+			  <p className="foot-container__text">© 2020 Kasa. All rights reserved</p>
+		  </footer>
+	  );
+  }
