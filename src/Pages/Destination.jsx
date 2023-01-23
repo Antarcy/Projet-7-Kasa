1019d013b9aeaec803b8609e32f55a6fa4730667
Louis LeGars <XYDistrict@gmail.com>
2023-01-23 14:15:27 +0100

    Clean carrousel

--- src/Pages/Destination.jsx
+++ src/Pages/Destination.jsx
@@ -19,63 +19,44 @@ export default function Destination() {
 		</li>
 	));
 	console.log(pickedAppart);
-	if(pickedAppart !== undefined) {
-	return (
-		<div key={params.id} className="fiche-container">
-			<Carrousel slides={slidePics} />
-			<section className="hostInfo-container">
-				<div className="title-tags-container">
-					<div className="title-container redFont">
-						<h1>{pickedAppart.title}</h1>
-						<h3>{pickedAppart.location}</h3>
+	if (pickedAppart !== undefined) {
+		return (
+			<div key={params.id} className="fiche-container">
+				<Carrousel slides={slidePics} />
+				<section className="hostInfo-container">
+					<div className="title-tags-container">
+						<div className="title-container redFont">
+							<h1>{pickedAppart.title}</h1>
+							<h3>{pickedAppart.location}</h3>
+						</div>
+						<div className="tags-container">
+							{tags.map((tag) => (
+								<Tag key={tag} tag={tag} />
+							))}
+						</div>
 					</div>
-					<div className="tags-container">
-						{tags.map((tag) => (
-							<Tag key={tag} tag={tag} />
-						))}
+					<div className="rate-host-container">
+						<div className="host-container redFont">
+							<Host
+								hostName={pickedAppart.host.name}
+								hostPic={pickedAppart.host.picture}
+							/>
+						</div>
+						<div className="rate-container">
+							<Rate score={pickedAppart.rating} />
+						</div>
 					</div>
+				</section>
+				<div className="collapse-fiche-container">
+					<Collapse
+						aboutTitle="Description"
+						aboutText={pickedAppart.description}
+					/>
+					<Collapse aboutTitle="Équipements" aboutText={equip} />
 				</div>
-				<div className="rate-host-container">
-					<div className="host-container redFont">
-						<Host
-							hostName={pickedAppart.host.name}
-							hostPic={pickedAppart.host.picture}
-						/>
-					</div>
-					<div className="rate-container">
-						<Rate score={pickedAppart.rating} />
-					</div>
-				</div>
-			</section>
-			<div className="collapse-fiche-container">
-				<Collapse
-					aboutTitle="Description"
-					aboutText={pickedAppart.description}
-				/>
-				<Collapse aboutTitle="Équipements" aboutText={equip} />
 			</div>
-		</div>
-	);
-						} else {
-							window.location.href ="/404";
-						}
+		);
+	} else {
+		window.location.href = "/404";
+	}
 }
-
-/*
-export default function LocationPage() {
-  const {id} = useParams();
-  const infoLocation = Data.find ((location)=> location.id === id)
-  if(pickedAppart !==undefined) {
-  const { title , location, tags , host, rating, pictures, description, equipments} = infoLocation
-  return (
-    <div>
-        <Carousel pictures={pictures}/>
-        <Info title={title} location= {location} tags={tags} host={host} rating={rating}/>
-        <Accordion description={description} equipments={equipments}/>
-    </div>
-  )
-  }else{
-    window.location.href ="/404";
-  }
-}
-*/
