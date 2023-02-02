8eb0d94872cebeeb276abb84b22e899c55caa42c
Louis LeGars <XYDistrict@gmail.com>
2023-02-02 12:53:55 +0100

    Last clear

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 		<div className="App">
 			<Header />
 			<main>
-				<Routes>
+				<Routes> {/* Rend les routes disponible dans tout le projet  */}
 					<Route path="/" element={<Home />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/logement/:id" element={<Destination />} />
