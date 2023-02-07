622afcf7417b364e1f414b5957f9b91482ec4853
Louis LeGars <XYDistrict@gmail.com>
2023-02-07 17:38:51 +0100

    Dernier fix (nav bar)

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,14 +1,24 @@
 import { NavLink } from "react-router-dom";
 
 export default function Nav() {
+
+  const currentRoute = window.location.pathname;
+
   return (
-    <nav className="nav-header">
-      <NavLink to="/" className="nav-header_link-home">
-        Accueil
-      </NavLink>
-      <NavLink to="/about" className="nav-header_link-about">
-        A Propos
-      </NavLink>
+    <nav className="nav">
+      <ul className='nav_list'>
+              <li className={currentRoute === '/Kasa/' ? 'nav_list_item_active' : 'nav_list_item'}>
+                  <NavLink  to='/Kasa/'>
+                      Accueil
+                  </NavLink>
+              </li>
+              <li className={currentRoute === '/about' ? 'nav_list_item_active' : 'nav_list_item'}>
+                  <NavLink  to='/about'>
+                      A propos
+                  </NavLink>
+              </li>
+          </ul>
     </nav>
   );
 }
+
